06cfecdc0394c800563cecaa809d58b8e8f708ec
Yang Liu <liuyang12@focusmedia.cn>
2019-08-07 17:18:52 +0800

    update

--- models/optimizer.py
+++ models/optimizer.py
@@ -1,15 +1,12 @@
-import numpy as np
-
-
 class Tacotron2Optimizer(object):
     """A simple wrapper class for learning rate scheduling"""
 
-    def __init__(self, optimizer, max_lr=1e-3, min_lr=1e-5, warmup_steps=50000, k=0.01):
+    def __init__(self, optimizer, k, embedding_dim, warmup_steps=4000):
         self.optimizer = optimizer
-        self.max_lr = max_lr
-        self.min_lr = min_lr
-        self.warmup_steps = warmup_steps
         self.k = k
+        self.init_lr = embedding_dim ** (-0.5)
+        self.lr = self.init_lr
+        self.warmup_steps = warmup_steps
         self.step_num = 0
 
     def zero_grad(self):
@@ -21,14 +18,10 @@ class Tacotron2Optimizer(object):
 
     def _update_lr(self):
         self.step_num += 1
-        if self.step_num > self.warmup_steps:
-            self.lr = self.max_lr * np.exp(-1.0 * self.k * self.step_num)
-            if self.lr >= self.min_lr:
-                for param_group in self.optimizer.param_groups:
-                    param_group['lr'] = self.lr
+        self.lr = self.k * self.init_lr * min(self.step_num ** (-0.5),
+                                              self.step_num * (self.warmup_steps ** (-1.5)))
+        for param_group in self.optimizer.param_groups:
+            param_group['lr'] = self.lr
 
-    def load_state_dict(self, state_dict):
-        self.optimizer.load_state_dict(state_dict)
-
-    def state_dict(self):
-        return self.optimizer.state_dict()
+    def set_k(self, k):
+        self.k = k
