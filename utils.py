537905f9b9033077e80d9a93076ab429d53fd900
Yang Liu <liuyang12@focusmedia.cn>
2019-08-07 15:54:15 +0800

    update

--- utils.py
+++ utils.py
@@ -6,6 +6,8 @@ import numpy as np
 import torch
 from scipy.io.wavfile import read
 
+from config import wave_folder
+
 
 def clip_gradient(optimizer, grad_clip):
     """
@@ -181,7 +183,7 @@ def load_wav_to_torch(full_path):
 
 def load_filepaths_and_text(filename, split="|"):
     with open(filename, encoding='utf-8') as f:
-        filepaths_and_text = [line.strip().split(split) for line in f]
+        filepaths_and_text = [line.strip().split(split).replace('DUMMY', wave_folder) for line in f]
     return filepaths_and_text
 
 
