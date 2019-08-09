47de64ee054bcf1bb2f941a642ecbe26b6a0e93b
Yang Liu <liuyang12@focusmedia.cn>
2019-08-09 16:43:17 +0800

    update

---
+++ demo.py
@@ -0,0 +1,34 @@
+import sys
+
+import matplotlib.pylab as plt
+
+sys.path.append('waveglow/')
+import numpy as np
+import torch
+
+from text import text_to_sequence
+
+
+def plot_data(data, figsize=(16, 4)):
+    fig, axes = plt.subplots(1, len(data), figsize=figsize)
+    for i in range(len(data)):
+        axes[i].imshow(data[i], aspect='auto', origin='bottom',
+                       interpolation='none')
+
+
+if __name__ == '__main__':
+    checkpoint = 'BEST_checkpoint.tar'
+    checkpoint = torch.load(checkpoint)
+    model = checkpoint['model']
+    model.eval()
+
+    sampling_rate = 22050
+
+    text = "Waveglow is really awesome!"
+    sequence = np.array(text_to_sequence(text, ['english_cleaners']))[None, :]
+    sequence = torch.autograd.Variable(torch.from_numpy(sequence)).cuda().long()
+
+    mel_outputs, mel_outputs_postnet, _, alignments = model.inference(sequence)
+    plot_data((mel_outputs.float().data.cpu().numpy()[0],
+               mel_outputs_postnet.float().data.cpu().numpy()[0],
+               alignments.float().data.cpu().numpy()[0].T))
